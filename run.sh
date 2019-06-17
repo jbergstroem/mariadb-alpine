30273891403159cc38772d23ba3d88bafb869cc9
Johan Bergström <johan@pageload.io>
2019-06-17 09:22:05 -0400

    init: only grant ownership if user is provided
    When bootstrapping a new database we unconditionally ran
    a `grant` query to assign ownership regardless if a user
    was passed or not. This would result in a error in style with:
    `ERROR: 1133  Can't find any matching row in the user table`.

--- run.sh
+++ run.sh
@@ -16,7 +16,7 @@ if [ -z "$(ls -A /var/lib/mysql/)" ]; then
   echo "INSERT INTO user VALUES ('%','root',${ROOTPW},'Y','Y','Y','Y','Y','Y','Y','Y','Y','Y','Y','Y','Y','Y','Y','Y','Y','Y','Y','Y','Y','Y','Y','Y','Y','Y','Y','Y','Y','Y','','','','',0,0,0,0,'','','N', 'N','', 0);" > /usr/share/mariadb/mysql_system_tables_data.sql
   /usr/bin/mysql_install_db --user=mysql --cross-bootstrap --rpm --skip-test-db --datadir=/var/lib/mysql
   [[ -n "${MYSQL_DATABASE}" ]] && echo "create database if not exists \`${MYSQL_DATABASE}\` character set utf8 collate utf8_general_ci; " >> /tmp/init
-  if [ -n "${MYSQL_USER}" -o -n "${MYSQL_DATABASE}" ]; then
+  if [ -n "${MYSQL_USER}" -a -n "${MYSQL_DATABASE}" ]; then
     echo "grant all on \`${MYSQL_DATABASE}\`.* to '${MYSQL_USER}'@'%' identified by '${MYSQL_PASSWORD}'; " >> /tmp/init
   fi 
   echo "flush privileges;" >> /tmp/init
