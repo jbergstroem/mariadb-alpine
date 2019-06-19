c9df4a6cf13863a9c3c45126d35574b93cb7f600
Johan Bergström <johan@pageload.io>
2019-06-19 13:11:39 -0400

    clean: add more plugins to remove

--- clean.sh
+++ clean.sh
@@ -9,39 +9,45 @@ set -eo pipefail
 
 # General stuff that can go. `mysqld_safe` gets picked up by grep;
 # so we need to remove it manually. Not sure how to work around this.
-GENERAL="/usr/share/terminfo
+GENERAL="
+	/usr/share/terminfo
 	/usr/bin/mysqld_safe"
 
 # Pointless plugins we want to remove because they're somewhat
-# bigger than most other ones, as well as config filews
-# we maintain separately
-PLUGINS="/usr/lib/mariadb/plugin/ha_connect.so
-    /usr/lib/mariadb/plugin/ha_spider.so"
+# bigger than most other ones. Also, remove examples
+PLUGINS="
+    /usr/lib/mariadb/plugin/dialog_examples.so
+    /usr/lib/mariadb/plugin/example_key_management.so
+	/usr/lib/mariadb/plugin/ha_connect.so
+    /usr/lib/mariadb/plugin/ha_example
+    /usr/lib/mariadb/plugin/ha_spider.so
+    /usr/lib/mariadb/plugin/handlersocket.so
+    /usr/lib/mariadb/plugin/libdaemon_example.so"
 
 # Things we'd like to keep. Double sed since busybox sed doesn't seem to
 # support '2g' which would skip the first match.
 KEEP=$(echo "usr/bin/mysqld
-	  usr/bin/getconf
-	  usr/bin/getent
-	  usr/bin/my_print_defaults
-	  usr/bin/mysql_install_db
-	  usr/share/mariadb/charsets
-	  usr/share/mariadb/english
-	  usr/share/mariadb/fill_help_tables.sql
-	  usr/share/mariadb/maria_add_gis_sp_bootstrap.sql
-	  usr/share/mariadb/mysql_test_db.sql
-	  usr/share/mariadb/mysql_performance_tables.sql
-	  usr/share/mariadb/mysql_system_tables.sql
-	  usr/share/mariadb/errmsg-utf8.txt
-	  usr/lib/mariadb" | sed -e 's/usr/|usr/g' -e 's/^.//' | tr -d " \t\n\r")
+	usr/bin/getconf
+	usr/bin/getent
+	usr/bin/my_print_defaults
+	usr/bin/mysql_install_db
+	usr/share/mariadb/charsets
+	usr/share/mariadb/english
+	usr/share/mariadb/fill_help_tables.sql
+	usr/share/mariadb/maria_add_gis_sp_bootstrap.sql
+	usr/share/mariadb/mysql_test_db.sql
+	usr/share/mariadb/mysql_performance_tables.sql
+	usr/share/mariadb/mysql_system_tables.sql
+	usr/share/mariadb/errmsg-utf8.txt
+	usr/lib/mariadb" | sed -e 's/usr/|usr/g' -e 's/^.//' | tr -d " \t\n\r")
 
 
 # Retrieve a list of files from `apk`, remove the header and finally 
 # exclude files/folders in $KEEP
 FILES="$(apk info -L mariadb | tail -n +2 | grep -v -E "${KEEP}")
-	   $(apk info -L mariadb-common | tail -n +2 | grep -v -E "${KEEP}")
-	   ${PLUGINS}
-	   ${GENERAL}"
+	$(apk info -L mariadb-common | tail -n +2 | grep -v -E "${KEEP}")
+	${PLUGINS}
+	${GENERAL}"
 
 # Finally, remove it all.
 #
