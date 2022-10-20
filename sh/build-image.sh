4c49cee3d161cb4e8802e765b370731d41e1b001
Johan Bergström <bugs@bergstroem.nu>
2022-10-20 18:05:17 -0300

    feat: create a manual release workflow
    The logic is as follows:
    1. Extract mariadb version from apk
    2. Build and test container on amd64
    3. Push to docker hub and github
    
    Options:
    - Ignore existing tags on github or docker hub. This disables
      sanity checks for not overwriting or replacing existing tags.
    - Push a new `:latest` tag.
    
    With this workflow, mariadb-alpine now supports 7 architectures:
    - linux/amd64
    - linux/arm/v6
    - linux/arm/v7
    - linux/arm64
    - linux/386
    - linux/s390x
    - linux/ppc64le
    
    Fixes: https://github.com/jbergstroem/mariadb-alpine/issues/29
    Fixes: https://github.com/jbergstroem/mariadb-alpine/issues/17

--- sh/build-image.sh
+++ sh/build-image.sh
@@ -4,9 +4,11 @@
 IMAGE=${IMAGE:-jbergstroem/mariadb-alpine}
 SHORT_SHA=$(git rev-parse --short HEAD)
 DATE=$(date -u +"%Y-%m-%dT%H:%M:%SZ")
+MARIADB_VERSION="$(sed -rn 's/^ARG APK_VERSION=\"([0-9]+\.[0-9]+\.[0-9]+).*\"$/\1/p' Dockerfile)"
 VERSION=${VERSION:-${SHORT_SHA}}
 
 docker image build \
   --build-arg BUILD_DATE="${DATE}" \
-  --build-arg VCS_REF="${SHORT_SHA}" \
+  --build-arg BUILD_REF="${SHORT_SHA}" \
+  --build-arg BUILD_VERSION="${MARIADB_VERSION}" \
   -t "${IMAGE}:${VERSION}" .
