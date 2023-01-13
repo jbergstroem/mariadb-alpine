4580238d063221e5f4319e08a57a8430101fe41b
Johan Bergström <bugs@bergstroem.nu>
2023-01-13 14:28:09 -0300

    fix: use same image for compose client

--- test/compose.sh
+++ test/compose.sh
@@ -1,15 +1,15 @@
 #!/usr/bin/env bash_unit
 # shellcheck shell=bash
 
-suite="mariadb-test-compose"
+# shellcheck source=test/_helper.bash
+. _helper.bash
 
-IMAGE_VERSION=${IMAGE_VERSION:-latest}
-CLIENT="docker run --network compose-secret_${suite} --rm jbergstroem/mariadb-client-alpine:latest"
+suite_name="mariadb-test-compose"
 
 setup_suite() {
   cd fixtures/compose-secret || exit 1
   VERSION="${IMAGE_VERSION}" docker compose up -d --no-build --no-log-prefix 2>/dev/null
-  until docker logs --tail 1 "${suite}" 2>&1 | grep -q "Version:"; do
+  until docker logs --tail 1 "${suite_name}" 2>&1 | grep -q "Version:"; do
     sleep 0.2
   done
 }
@@ -19,10 +19,10 @@ teardown_suite() {
 }
 
 test_root_password_secret() {
-  assert "${CLIENT} -h ${suite} --connect-timeout=5 --user=root --password=secret -e 'select 1;'"
+  assert "${CLIENT} -h ${suite_name} --connect-timeout=5 --user=root --password=secret -e 'select 1;'"
 }
 
 test_user_password_secret_override() {
-  assert "${CLIENT} -h ${suite} --connect-timeout=5 --user=foo --database=db --password=override -e 'select 1;'"
-  assert_fail "${CLIENT} -h ${suite} --connect-timeout=5 --user=foo --database=db --password=password -e 'select 1;'"
+  assert "${CLIENT} -h ${suite_name} --connect-timeout=5 --user=foo --database=db --password=override -e 'select 1;'"
+  assert_fail "${CLIENT} -h ${suite_name} --connect-timeout=5 --user=foo --database=db --password=password -e 'select 1;'"
 }
