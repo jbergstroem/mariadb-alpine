917a040e71cfc9f3343cf25d6bb01cd3ca21df5f
Johan Bergström <bugs@bergstroem.nu>
2023-01-19 09:38:34 -0300

    feat: sort benchmark by size
    Fixes: https://github.com/jbergstroem/mariadb-alpine/issues/187

--- sh/generate-benchmark.sh
+++ sh/generate-benchmark.sh
@@ -4,7 +4,6 @@ set -euo pipefail
 
 # Fetches latest version and compressed size from dockerhub
 # TODO Benchmark should be run as part of release engineering/automation
-# TODO Sort benchmark by container size
 # TODO Fetch latest tag instead of digest for containers in benchmark
 # TODO Add size factor compared to smallest container in benchmark
 
@@ -17,8 +16,6 @@ declare -A images=(
   ["**jbergstroem/mariadb-alpine**"]="jbergstroem/mariadb-alpine"
 )
 
-output=("| image name | size | digest (version) |")
-output+=("|:--|:--|:--|")
 for image in "${!images[@]}"; do
   result=$(curl -L -s "https://hub.docker.com/v2/repositories/${images[${image}]}/tags?page_size=100" | jq '.results[] | select(.name =="latest")')
   size=$(echo "${result}" | jq '.full_size' | numfmt --from=iec --to=iec-i --format "%.1f")
@@ -27,4 +24,6 @@ for image in "${!images[@]}"; do
   output+=("| ${image} | ${size} | [${short_digest}](https://hub.docker.com/layers/${images[${image}]}/latest/images/${digest}?context=explore) |")
 done
 
-printf "%s\n" "${output[@]}"
+echo "| image name | size | digest (version) |"
+echo "|:--|:--|:--|"
+printf "%s\n" "${output[@]}" | sort -t '|' -k 3 -h
