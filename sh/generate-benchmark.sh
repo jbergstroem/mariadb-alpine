130f8d36dc3224ce8541142ff0420526df472c5f
Johan Bergström <bugs@bergstroem.nu>
2023-01-24 12:08:14 -0300

    feat: show daemon version in benchmark
    Fixes: https://github.com/jbergstroem/mariadb-alpine/issues/188

--- sh/generate-benchmark.sh
+++ sh/generate-benchmark.sh
@@ -2,9 +2,8 @@
 # shellcheck shell=bash
 set -euo pipefail
 
-# Fetches latest version and compressed size from dockerhub
+# Fetches images, extracts daemon version and container size
 # TODO Benchmark should be run as part of release engineering/automation
-# TODO Fetch latest tag instead of digest for containers in benchmark
 
 declare -A images=(
   ["mysql"]="library/mysql"
@@ -16,13 +15,19 @@ declare -A images=(
 )
 
 for image in "${!images[@]}"; do
-  result=$(curl -L -s "https://hub.docker.com/v2/repositories/${images[${image}]}/tags?page_size=100" | jq '.results[] | select(.name =="latest")')
-  size=$(echo "${result}" | jq '.full_size' | numfmt --from=iec --to=iec-i --format "%.1f")
-  digest=$(echo "${result}" | jq -r '.digest')
-  short_digest=$(echo "${result}" | jq -r '.digest | sub("sha256:";"") | .[0:8]')
-  output+=("| ${image} | ${size} | [${short_digest}](https://hub.docker.com/layers/${images[${image}]}/latest/images/${digest}?context=explore) |")
+  entrypoint=mariadbd
+  [[ "${image}" == "mysql" ]] && entrypoint="mysqld"
+  docker pull -q "${images[$image]}:latest" >/dev/null 2>&1
+  version=$(docker run -it --rm --entrypoint "${entrypoint}" "${images[$image]}:latest" --version)
+  [[ "${version}" =~ Ver[[:space:]]([0-9]+.[0-9]+.[0-9]+) ]]
+  parsed_version="${BASH_REMATCH[1]}"
+  compressed_size=$(curl -L -s "https://hub.docker.com/v2/repositories/${images[${image}]}/tags?page_size=100" | jq '.results[] | select(.name =="latest") | .full_size' | numfmt --from=iec --to=iec-i --format "%.1f")
+  size=$(docker image inspect -f "{{.Size}}" "${images[$image]}" | numfmt --from=iec --to=iec-i --format "%.1f")
+  output+=("| ${image} | ${compressed_size} (${size}) | [${parsed_version}](https://hub.docker.com/layers/${images[$image]}/latest/images/latest?context=explore) |")
+  # some images may already exist on disk and be in use, we don't care if pruning fails
+  docker rmi "${images[$image]}" >/dev/null 2>&1 || true
 done
 
-echo "| image name | size | digest (version) |"
+echo "| Image name | Compressed size (Original) | Version |"
 echo "|:--|:--|:--|"
 printf "%s\n" "${output[@]}" | sort -t '|' -k 3 -h
